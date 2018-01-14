ac55218fba3b4b0dcde8a8f4e5a86e4e3f981387
Wendy Nie <wendy90n@gmail.com>
2018-01-14 00:35:40 -0800

    bg color
    adjust and save bg color

--- popup.js
+++ popup.js
@@ -52,7 +52,8 @@ function getCurrentTabUrl(callback) {
  *
  * @param {string} color The new background color.
  */
-function changeBackgroundColor(color) {
+function changeBackgroundColor(r, g, b) {
+    color = "rgb(" + r + "," + g + "," + b + ")";
   var script = `color = "` + color + `";
 console.log("Changing background color...");
 document.body.style.backgroundColor=color;
@@ -68,6 +69,12 @@ for (var el of els) {
   chrome.tabs.executeScript({
     code: script
   });
+}
+
+function setBgColorInExt(r, g, b) {
+    color = "rgb(" + r + "," + g + "," + b + ")";
+    document.getElementById("left_eye_test").style.backgroundColor = color;
+    document.getElementById("right_eye_test").style.backgroundColor = color;
 }
 
 function setFontSize(val) {
@@ -88,18 +95,10 @@ function setFontSizeInExt(size) {
     right_eye_test.style.fontSize = size + "px";
     var left_eye_test = document.getElementById('left_eye_test');
     left_eye_test.style.fontSize = size + "px";
-}
-
-function increaseFontSizeInExt(settings) {
-    settings.font_size += 4;
-    setFontSizeInExt(settings.font_size);
-}
-
-function decreaseFontSizeInExt(settings) {
-    if (settings.font_size >= 8) {
-        settings.font_size -= 4;
-        setFontSizeInExt(settings.font_size);
-    }
+    var script = `console.log("calling setFontSizeInExt");`
+    chrome.tabs.executeScript({
+        code: script
+    });
 }
 
 function changeFontColor(colors_str) {
@@ -145,7 +144,11 @@ for (var el of els) {
 }
 
 function settingsToString(settings) {
-    return settings.colors_on + "|" + settings.font_size;
+    return settings.colors_on + "|" + 
+        settings.font_size + "|" + 
+        settings.bg_r + "|" + 
+        settings.bg_g + "|" + 
+        settings.bg_b;
 }
 
 function stringToSettings(str) {
@@ -153,6 +156,9 @@ function stringToSettings(str) {
     var arr = str.split('|');
     settings.colors_on = arr[0] == 'true';
     settings.font_size = parseInt(arr[1]);
+    settings.bg_r = parseInt(arr[2]);
+    settings.bg_g = parseInt(arr[3]);
+    settings.bg_b = parseInt(arr[4]);
     return settings;
 }
 
@@ -161,7 +167,10 @@ function getSavedOrDefaultSettings(hostname, callback) {
     chrome.storage.sync.get(hostname, (items) => {
         var default_settings = {
             font_size: 20,
-            colors_on: false
+            colors_on: false,
+            bg_r: 25,
+            bg_g: 25,
+            bg_b: 25
         }
         callback(chrome.runtime.lastError ? default_settings : (items[hostname] === undefined ? default_settings : stringToSettings(items[hostname])));
     });
@@ -195,32 +204,76 @@ document.addEventListener('DOMContentLoaded', () => {
     var hostname = parser.hostname;
     document.getElementById("domain").innerHTML = hostname;
       
+    var font_size_slider = document.getElementById("font_size_slider");
+    var selected_font_size = document.getElementById("selected_font_size");
+      
+    var bg_r_slider = document.getElementById("bg_r_slider");  
+    var bg_g_slider = document.getElementById("bg_g_slider");  
+    var bg_b_slider = document.getElementById("bg_b_slider"); 
+    var selected_bg_r = document.getElementById("selected_bg_r");  
+    var selected_bg_g = document.getElementById("selected_bg_g");  
+    var selected_bg_b = document.getElementById("selected_bg_b"); 
+      
     // Load the saved settings for this host.
     getSavedOrDefaultSettings(hostname, (savedSettings) => {
         settings.colors_on = savedSettings.colors_on;
         settings.font_size = savedSettings.font_size;
+        settings.bg_r = savedSettings.bg_r;
+        settings.bg_g = savedSettings.bg_g;
+        settings.bg_b = savedSettings.bg_b;
         setFontSize(savedSettings.font_size);
         setFontSizeInExt(savedSettings.font_size);
+        setBgColorInExt(settings.bg_r, settings.bg_g, settings.bg_b);
+        selected_bg_r.innerHTML = settings.bg_r;
+        selected_bg_g.innerHTML = settings.bg_g;
+        selected_bg_b.innerHTML = settings.bg_b;
+        font_size_slider.value = savedSettings.font_size;
+        selected_font_size.innerHTML = savedSettings.font_size;
         if (savedSettings.colors_on) {
-            changeBackgroundColor('#191919');
+            changeBackgroundColor(settings.bg_r, settings.bg_g, settings.bg_b);
             changeFontColor('#7D0000|#00007D');
         }
     });
-      
-    var incr_font_size = document.getElementById('incr_font_size');
-    incr_font_size.addEventListener('click', () => {
-        increaseFontSizeInExt(settings);
+    
+    font_size_slider.addEventListener('change', () => {
+        settings.font_size = font_size_slider.value;
+        selected_font_size.innerHTML = settings.font_size;
+        setFontSizeInExt(settings.font_size);
     });
       
-    var decr_font_size = document.getElementById('decr_font_size');
-    decr_font_size.addEventListener('click', () => {
-        decreaseFontSizeInExt(settings);
+    bg_r_slider.addEventListener('change', () => {
+        settings.bg_r = bg_r_slider.value;
+        setBgColorInExt(bg_r_slider.value, bg_g_slider.value, bg_b_slider.value);
+    });
+      
+    bg_r_slider.addEventListener('input', () => {
+        selected_bg_r.innerHTML = bg_r_slider.value
+    });
+      
+    bg_g_slider.addEventListener('change', () => {
+        settings.bg_g = bg_g_slider.value;
+        selected_bg_g.innerHTML = settings.bg_g;
+        setBgColorInExt(bg_r_slider.value, bg_g_slider.value, bg_b_slider.value);
+    });
+      
+    bg_g_slider.addEventListener('input', () => {
+        selected_bg_g.innerHTML = bg_g_slider.value
+    });
+      
+    bg_b_slider.addEventListener('change', () => {
+        settings.bg_b = bg_b_slider.value;
+        selected_bg_b.innerHTML = settings.bg_b;
+        setBgColorInExt(bg_r_slider.value, bg_g_slider.value, bg_b_slider.value);
+    });
+    
+    bg_b_slider.addEventListener('input', () => {
+        selected_bg_b.innerHTML = bg_b_slider.value
     });
       
     var update_page = document.getElementById('update_page');
     update_page.addEventListener('click', () => {
         setFontSize(settings.font_size);
-        changeBackgroundColor('#191919');
+        changeBackgroundColor(settings.bg_r, settings.bg_g, settings.bg_b);
         changeFontColor('#7D0000|#00007D');
         settings.colors_on = true;
     });
